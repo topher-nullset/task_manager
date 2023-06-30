5690f398c7d6cc53005bff154755ee808c97cebf
Chris Cullinane <chriscullinane@protonmail.com>
2023-06-30 13:45:43 -0800

    task managin

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/", to: "welcome#index"
+
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show"
+  get "/tasks/:id/edit", to: "tasks#edit"
+  patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#destroy"
 end
